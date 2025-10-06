3024c07bc2f6ded26b8219f57e250cecb130e9c7
Ivan Betancourt <ivanab252@gmail.com>
2025-10-05 23:42:55 -0400

    Add grid encode method

---
+++ nca.py
@@ -0,0 +1,24 @@
+import torch as th
+
+class NCA(th.nn.Module):
+    def __init__(self, n_hidden_channels: int = 20, device: th.device = None) -> None:
+        super().__init__()
+        self.n_hidden_channels = n_hidden_channels
+        self.n_channels = 10 + n_hidden_channels
+        self.device = device or th.device('cpu')
+
+    def encode(self, grid: th.LongTensor) -> th.FloatTensor:
+        '''
+            Converts ARC grid (2D tensor of values 0-9) into model-readable format (H x W x (10 + hidden channels)).
+
+            Args: 
+                grid (2D LongTensor): ARC grid
+
+            Returns:
+                (3D FloatTensor): One hot representation of the color channels concatenated with hidden channels.
+        '''
+        one_hot_grid = th.nn.functional.one_hot(grid, num_classes=10)
+        hidden_channels = th.zeros(grid.shape[0], grid.shape[1], self.n_hidden_channels)
+        return th.cat((one_hot_grid, hidden_channels), dim=2)
+
+    
