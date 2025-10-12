0e8b3d636d48ec904393d9ad5bc7b7ced57178b1
Ivan Betancourt <ivanab252@gmail.com>
2025-10-12 16:27:29 -0400

    Clean device code + draft forward method

--- nca.py
+++ nca.py
@@ -1,27 +1,30 @@
 import torch as th
 
 class NCA(th.nn.Module):
-    def __init__(self, n_hidden_channels: int = 20, device: th.device = None) -> None:
+    def __init__(self, n_hidden_channels: int = 20,) -> None:
         super().__init__()
         self.n_hidden_channels = n_hidden_channels
         self.n_channels = 10 + n_hidden_channels
-        self.device = device or th.device('cpu') #? this okay?
         self.perceive = th.nn.Conv2d(in_channels=self.n_channels, out_channels=64, kernel_size=3, padding=1) #! hard-coded output channels
         self.norm = th.nn.GroupNorm(num_groups=1, num_channels=64) #! hard-coded output channels
         self.update = th.nn.Conv2d(in_channels=64, out_channels=self.n_channels, kernel_size=1) #! hard-coded input channels
 
     def encode(self, grid: th.LongTensor) -> th.FloatTensor:
         '''
-            Converts ARC grid (2D tensor of values 0-9) into model-readable format (1 x (10 + hidden channels) x H x W).
+            Converts ARC grid (2D tensor of values 0-9) into model-readable format (N x (10 + hidden channels) x H x W).
         '''
-        one_hot_grid = th.nn.functional.one_hot(grid, num_classes=10).to(self.device)
-        hidden_channels = th.zeros(grid.shape[0], grid.shape[1], self.n_hidden_channels).to(self.device)
+        one_hot_grid = th.nn.functional.one_hot(grid, num_classes=10).float()
+        hidden_channels = th.zeros(grid.shape[0], grid.shape[1], self.n_hidden_channels, device=grid.device)
         encoded_grid = th.cat((one_hot_grid, hidden_channels), dim=2)
         return encoded_grid.permute(2, 0, 1).unsqueeze(0)
 
     def decode(self, grid: th.FloatTensor) -> th.LongTensor:
         '''
-            Converts an intermediary grid back into ARC format (H x W).
+            Converts an intermediary grid back into ARC format (N x H x W).
         '''
-        color_channels = grid.permute(0, 2, 3, 1).squeeze()[:, :, :10]
-        return th.argmax(color_channels, dim=2)
+        return grid[:, :10].argmax(dim=1)
+
+    def forward(self, x: th.FloatTensor) -> th.FloatTensor:
+        y = self.perceive(x)
+        y = self.norm(y)
+        return self.update(y)
