20f3d85b46279ed21327738985d10105806c5163
Ivan Betancourt <ivanab252@gmail.com>
2025-11-05 00:49:19 -0500

    Add slurm job script

---
+++ train.py
@@ -0,0 +1,11 @@
+from torch.utils import data
+from nca import NCA
+import torch as th
+
+device = th.device('cuda' if th.cuda.is_available() else 'cpu')
+
+print(f'DEVICE: {device}')
+model = NCA()
+model = model.to(device)
+
+model.fit(data_directory='data/arc-1/training', epochs=3)
