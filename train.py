303dbc1bf5e58dd906d6252694fcf76d164086aa
Ivan Betancourt <ivanab252@gmail.com>
2025-11-09 23:20:53 -0500

    Save model on successful training

--- train.py
+++ train.py
@@ -9,3 +9,15 @@ model = NCA()
 model = model.to(device)
 
 model.fit(data_directory='../data/arc-1/training', epochs=3)
+
+th.save({
+    'model': model.state_dict(),
+    'configs': {
+        'n_hidden_channels': model.n_hidden_channels,
+        'out_channels': 64
+    },
+    'epochs': 3,
+    'device': str(device)
+}, '../checkpoints/nca_test_02.pth')
+
+print('Model saved.')
