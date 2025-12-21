5f5cb89c80861b0f953278bfbe77bd4c7527924b
Ivan Betancourt <ivanab252@gmail.com>
2025-12-21 15:55:44 -0500

    Complete full model evaluation script

--- evaluate.py
+++ evaluate.py
@@ -15,55 +15,76 @@ def main():
     device = 'cuda' if th.cuda.is_available() else 'cpu'
 
     data = []
-    if args.bytask:
-        num_tasks = len(os.listdir(f'../data/{args.dataset}/training'))
+    fieldnames = [
+        'task', 
+        'solved', 
+        'final_pixel_accuracy', 
+        'n_hidden_channels',
+        'temperature',
+        'steps',
+        'trials',
+        'learning_rate',
+        'mask_prob_low',
+        'mask_prob_high'
+    ]
 
+    def evaluate(model: NCA, configs: dict, task_num: int, dataset: str):
+        '''
+            Evaluate the model on the specified task.
+        '''
+        with open(f'../data/{dataset}/training/task_{task_num}.json', 'r') as f:
+            task = json.load(f)['test'][0]
+            
+        x = th.tensor(task['input'])
+        y = th.tensor(task['output'])
+
+        res = model.evaluate(inputs=x.unsqueeze(0), targets=y.unsqueeze(0))
+
+        data.append({
+            'task': task_num,
+            'solved': 'True' if res['exact_match_final_accuracy'] == 1.0 else 'False',
+            'final_pixel_accuracy': res['pixel_final_accuracy'],
+            'n_hidden_channels': configs['n_hidden_channels'],
+            'temperature': configs['temperature'],
+            'steps': configs['steps'],
+            'trials': configs['trials'],
+            'learning_rate': configs['learning_rate'],
+            'mask_prob_low': configs['mask_prob_low'],
+            'mask_prob_high': configs['mask_prob_high']
+        })
+
+    num_tasks = len(os.listdir(f'../data/{args.dataset}/training'))
+    if args.bytask:
         for n in range(1, num_tasks + 1):
             ckpt = th.load(f'../checkpoints/{args.dataset}_bytask_0{args.run}/{args.dataset}_bytask{n}_0{args.run}.pth', map_location=th.device(device))
             configs = ckpt['configs']
             state = ckpt['model']
             model = NCA()
             model.load_state_dict(state)
+            model.to(device)
 
-            with open(f'../data/{args.dataset}/training/task_{n}.json', 'r') as f:
-                task = json.load(f)['test'][0]
-            
-            x = th.tensor(task['input'])
-            y = th.tensor(task['output'])
-
-            res = model.evaluate(inputs=x.unsqueeze(0), targets=y.unsqueeze(0))
-
-            fieldnames = [
-                'task', 
-                'solved', 
-                'final_pixel_accuracy', 
-                'n_hidden_channels',
-                'temperature',
-                'steps',
-                'trials',
-                'learning_rate',
-                'mask_prob_low',
-                'mask_prob_high'
-            ]
-
-            data.append({
-                'task': n,
-                'solved': 'True' if res['exact_match_final_accuracy'] == 1.0 else 'False',
-                'final_pixel_accuracy': res['pixel_final_accuracy'],
-                'n_hidden_channels': configs['n_hidden_channels'],
-                'temperature': configs['temperature'],
-                'steps': configs['steps'],
-                'trials': configs['trials'],
-                'learning_rate': configs['learning_rate'],
-                'mask_prob_low': configs['mask_prob_low'],
-                'mask_prob_high': configs['mask_prob_high']
-            })
+            evaluate(model=model, configs=configs, task_num=n, dataset=args.dataset)
 
         with open(f'../data/results/results_{args.dataset}_bytask_0{args.run}.csv', 'w', newline='', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=fieldnames)
             writer.writeheader()  
             writer.writerows(data)
+            
+    else:
+        ckpt = th.load(f'../checkpoints/{args.dataset}_full_0{args.run}.pth', map_location=th.device(device))
+        configs = ckpt['configs']
+        state = ckpt['model']
+        model = NCA()
+        model.load_state_dict(state)
+        model.to(device)
 
+        for n in range(1, num_tasks + 1):
+            evaluate(model=model, configs=configs, task_num=n, dataset=args.dataset)
+
+        with open(f'../data/results/results_{args.dataset}_full_0{args.run}.csv', 'w', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=fieldnames)
+            writer.writeheader()  
+            writer.writerows(data)
 
 if __name__ == '__main__':
     main()
