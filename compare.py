d685ae0d3779e4cba13114b8d268ad24ffd9b515
Ivan Betancourt <ivanab252@gmail.com>
2025-12-28 21:24:26 -0500

    Create comparison script

--- compare.py
+++ compare.py
@@ -1,8 +1,30 @@
 import argparse
+import csv
 
-def base_to_lexi():
+def base_to_lexi(dataset: str, base_run: int, lexi_run: int, generations: int, epsilon: float):
+    data = []
     fieldnames = ['task', 'accuracy_delta']
 
+    with open(f'data/results/{dataset}_full/{dataset}_full_0{base_run}_results.csv', newline="", encoding="utf-8") as f: #! LEADING 0
+        base_results = list(csv.DictReader(f))
+
+    with open(
+        f'data/results/{dataset}_full_lexi/{dataset}_full_lexi_{lexi_run}_({generations}g_{epsilon}e)_results.csv', 
+        newline="", 
+        encoding="utf-8"
+    ) as f:
+        lexi_results = list(csv.DictReader(f))
+    
+    for (base_row, lexi_row) in zip(base_results, lexi_results):
+        data.append({
+            'task': base_row['task'],
+            'accuracy_delta': float(lexi_row['final_pixel_accuracy']) - float(base_row['final_pixel_accuracy'])
+        })
+    
+    with open(f'data/results/comparisons/base_to_lexi/{base_run}_to_{lexi_run}_({generations}g_{epsilon}e).csv', 'w', newline="", encoding="utf-8") as f:
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()  
+        writer.writerows(data)
 
 
 if __name__ == '__main__':
@@ -15,3 +37,14 @@ if __name__ == '__main__':
     base_to_lexi_parser.add_argument('--lexirun', type=int, help='Run number of lexi model')
     base_to_lexi_parser.add_argument('--gens', type=int, help='Number of generations lexi model was evolved')
     base_to_lexi_parser.add_argument('--epsilon', type=float, help='Survival threshold of lexi model')
+
+    args = parser.parse_args()
+
+    if args.command == 'baselexi':
+        base_to_lexi(
+            dataset=args.dataset,
+            base_run=args.baserun,
+            lexi_run=args.lexirun,
+            generations=args.gens,
+            epsilon=args.epsilon
+        )
